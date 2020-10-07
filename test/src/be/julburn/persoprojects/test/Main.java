e926665951d3f692b24bcc3e5a5f40a059bf03e2
Julien-76 <julien.pothen@gmail.com>
2020-10-07 23:54:43 +0200

    init

---
+++ test/src/be/julburn/persoprojects/test/Main.java
@@ -0,0 +1,10 @@
+package be.julburn.persoprojects.test;
+
+public class Main{
+	public static void main(String... args){
+		System.out.println("Tout plein de bisous ! ! ! ! ! <3 <3 <3 <3 <3");
+	}
+
+
+
+}
